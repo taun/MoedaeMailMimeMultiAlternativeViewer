9d32862e3ddf8e7e78c820ac026096f9554c1ef6
Taun Chapman <tchapman@moedae.com>
2014-02-13 13:05:09 -0500

    Intial import

---
+++ MoedaeMailMimeMultiAlternativeViewer/MMPMMultiAlternative.h
@@ -0,0 +1,13 @@
+//
+//  MMPMMultiAlternative.h
+//  MoedaeMailMimeMultiAlternativeViewer
+//
+//  Created by Taun Chapman on 02/08/14.
+//  Copyright (c) 2014 MOEDAE LLC. All rights reserved.
+//
+
+#import <MoedaeMailPlugins/MoedaeMailPlugins.h>
+
+@interface MMPMMultiAlternative : MMPBaseMimeView
+
+@end
---
+++ MoedaeMailMimeMultiAlternativeViewer/MMPMMultiAlternative.m
@@ -0,0 +1,60 @@
+//
+//  MMPMMultiAlternative.m
+//  MoedaeMailMimeMultiAlternativeViewer
+//
+//  Created by Taun Chapman on 02/08/14.
+//  Copyright (c) 2014 MOEDAE LLC. All rights reserved.
+//
+
+#import "MMPMMultiAlternative.h"
+
+@implementation MMPMMultiAlternative
+
++(NSSet*) contentTypes {
+    return [NSSet setWithObjects:@"MULTIPART/ALTERNATIVE", nil];
+}
+
+-(void) createSubviews {
+    NSOrderedSet* subNodes = self.node.childNodes;
+    
+    MMPMimeProxy* plainText;
+    MMPMimeProxy* richNode; // can be html or enriched
+    
+    for (MMPMimeProxy* node in subNodes) {
+        // should only be two nodes, use an assert?
+        // could also just check the first node
+        // rather than depend on the order of childnodes, let's find the plain text alternative.
+        if ([node.type isEqualToString:@"TEXT"] && [node.subtype isEqualToString: @"PLAIN"]) {
+            plainText = node;
+        } else {
+            richNode = node;
+        }
+    }
+    
+    MMPMimeProxy* node = [self wantsRichTextPresentation] ? richNode : plainText;
+    
+    Class nodeViewClass = [[MBMimeViewerPluginsManager manager] classForMimeType: node.type subtype: node.subtype];
+    
+    NSSize subStructureSize = self.frame.size;
+    NSRect nodeRect = NSMakeRect(0, 0, subStructureSize.width, subStructureSize.height);
+    MMPBaseMimeView* nodeView = [[nodeViewClass alloc] initWithFrame: nodeRect node: node options: self.options attributes: self.attributes];
+
+    nodeView.options = self.options;
+    nodeView.attributes = self.attributes;
+    
+    self.mimeView = nodeView;
+    [self addSubview: nodeView];
+    
+    [nodeView setTranslatesAutoresizingMaskIntoConstraints: NO];
+    
+    [nodeView removeConstraints: nodeView.constraints];
+    [self removeConstraints: self.constraints];
+    [self setNeedsUpdateConstraints: YES];    
+}
+
+-(void) dealloc {
+//    NSNotificationCenter* nc = [NSNotificationCenter defaultCenter];
+//    [nc removeObserver: self.mimeView];
+}
+
+@end
